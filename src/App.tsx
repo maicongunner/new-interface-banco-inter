e340ecb8f8e0a3c48aecf2d4a5d4121dd801d38c
Maicon Domingues <maikonbdomingues@gmail.com>
2020-08-12 08:23:25 -0300

    impr: configure default theme and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import theme from './styles/theme';
+import GlobalStyles from './styles/global';
 
 const App: React.FC = () => {
   return (
-    <h1>Hello World 2!</h1>
+    <ThemeProvider theme={theme}>
+      <h1>Hello World 2!</h1>
+      <GlobalStyles />
+    </ThemeProvider>
   );
 }
 
