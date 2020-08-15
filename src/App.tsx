83f32edcffef79c5efda281e94e37dcad1c2c957
Maicon Domingues <maikonbdomingues@gmail.com>
2020-08-15 16:58:11 -0300

    feat: add gradient block on header

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import Header from './components/Header';
 
 import theme from './styles/theme';
 import GlobalStyles from './styles/global';
+import Gradient from './components/Gradient';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <Header />
+      <Gradient />
       <GlobalStyles />
     </ThemeProvider>
   );
